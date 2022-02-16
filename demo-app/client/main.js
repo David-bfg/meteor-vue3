1a10894619c01c7a777e67aede2f2828de8774a7
david-bfg <david.1.bfg@gmail.com>
2022-02-16 15:55:43 -0600

    Remove blaze for static html and components are now inaccessible.

--- demo-app/client/main.js
+++ demo-app/client/main.js
@@ -1,4 +1,12 @@
 // Client entry point, imports all client code
 
-import '/imports/startup/client';
+// import '/imports/startup/client';
 import '/imports/startup/both';
+
+import { createApp } from 'vue'
+import App from '../imports/ui/components/App.vue'
+
+Meteor.startup(() => {
+  const app = createApp(App);
+  app.mount('#app');
+});
--- demo-app/imports/ui/components/App.vue
+++ demo-app/imports/ui/components/App.vue
@@ -1,15 +1,8 @@
-<script>
+<script setup>
 import Counter from './Counter.vue'
 import ScriptSetup from './ScriptSetup.vue'
 import Info from './Info.vue'
 
-export default {
-  components: {
-    Counter,
-    ScriptSetup,
-    Info,
-  },
-}
 </script>
 
 <template>
